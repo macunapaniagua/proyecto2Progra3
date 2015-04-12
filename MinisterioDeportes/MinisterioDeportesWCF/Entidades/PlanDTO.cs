9775663a57a652387ccbc384dbc2421f8ea0495a
isaCR <isagonzacr@gmail.com>
2015-04-12 14:02:26 -0600

    dto

---
+++ MinisterioDeportes/MinisterioDeportesWCF/Entidades/PlanDTO.cs
@@ -0,0 +1,28 @@
+﻿using MinisterioDeportesAccesoADatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MinisterioDeportesWCF.Entidades
+{
+    [DataContract]
+    public class PlanDTO
+    {
+        public PlanDTO(plan plan)
+            {
+                this.descripcion = plan.descripcion;
+                this.detalles = plan.detalles;
+                this.id = Convert.ToInt16(plan.ID);
+
+            }
+
+            [DataMember]
+            public string descripcion { get; set; }
+            [DataMember]
+            public string detalles { get; set; }
+            [DataMember]
+            public Int16 id { get; set; }
+    }
+}
---
+++ MinisterioDeportes/MinisterioDeportesWCF/Entidades/RutinaDTO.cs
@@ -0,0 +1,31 @@
+﻿using MinisterioDeportesAccesoADatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MinisterioDeportesWCF.Entidades
+{
+    [DataContract]
+    public class RutinaDTO
+    {
+
+        public RutinaDTO(rutina rutina)
+            {
+                this.descripcion = rutina.descripcion;
+                this.nombre = rutina.nombre;
+                this.id = Convert.ToInt16(rutina.id);
+
+            }
+
+            [DataMember]
+            public string descripcion { get; set; }
+            [DataMember]
+            public string nombre { get; set; }
+            [DataMember]
+            public Int16 id { get; set; }
+
+       
+    }
+}
